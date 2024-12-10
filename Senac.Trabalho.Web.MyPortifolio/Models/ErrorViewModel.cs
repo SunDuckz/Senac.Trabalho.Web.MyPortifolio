124ecaafa752269e43ff0179f6c7234e1210ba2d
SunDuckz <gui.bampi0@gmail.com>
2024-12-09 21:30:26 -0300

    Adicionar arquivos de projeto.

---
+++ Senac.Trabalho.Web.MyPortifolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Senac.Trabalho.Web.MyPortifolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
